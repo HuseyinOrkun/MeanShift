90377fcedaae33f5037b7424db5d782a3f18cd7b
HuseyinOrkun <huseyinorkunelmas@gmail.com>
2017-12-16 19:32:18 +0400

    not working

--- SegmentImage.m
+++ SegmentImage.m
@@ -1,18 +1,18 @@
 function [labels peaks] = SegmentImage(img,params)
 % use for test in small data subset 
-data = test();
-[n,d] = size(data);
-%[x,y,d] = size(img);
-%n = x*y; %points vs data
-% data = zeros(n,d);
-% for i = 1:x
-%     for j = 1:y
-%        data((i-1)*y+j,:) = img(i,j,:) ; % I shoud append i,j in here
-%     end
-% end
+% data = test();
+% [n,d] = size(data);
+[x,y,d] = size(img);
+n = x*y; %points vs data
+data = zeros(n,d);
+for i = 1:x
+    for j = 1:y
+       data((i-1)*y+j,:) = img(i,j,:) ; % I shoud append i,j in here
+    end
+end
 r = params(2);
 peaks_temp = zeros([n,d]) - 1;
-clusters = zeros([n,1]) - 1;
+clusters = zeros([n,1]);
 notUsedFlag = zeros([1,d]) - 1;
 % Implement Neighborhood thingy
 % Selim hoca baz?lar?n? atmay?p aras?na label bulunabilir demi?ti, nas?l?
@@ -32,8 +32,8 @@ for i= 1:n
             % Compare all peaks and merge two peaks in one cluster if they are
             % closer than r/2 
             notFoundFlag = true;
-            for j = 1:n 
-                if ~isequal(peaks_temp(j,:),notUsedFlag)&&(euclidean_dist(peaks_temp(j,:),new_peak) <= r/2)
+            for j = i:n 
+                if ~isequal(peaks_temp(j,:),notUsedFlag)&&(euclidean_dist(peaks_temp(j,:),new_peak) < r/2)
                     clusters(j) = i;
                     notFoundFlag = false;
                 end
@@ -47,21 +47,15 @@ unique_clusters = unique(clusters(clusters>0))
 for i = 1:size(unique_clusters,1)
     peaks(i,:) =  mean(peaks_temp(clusters == unique_clusters(i),:));
 end
-scatter(peaks(:,1),peaks(:,2),"Marker",'+',"MarkerFaceColor",[1,0,0]);pause;
+%scatter(peaks(:,1),peaks(:,2),"Marker",'+',"MarkerFaceColor",[1,0,0]);pause;
 % %scatter(peaks(:,1),peaks(:,2),"Marker",'+',"MarkerFaceColor",[0,1,1]);pause;
-% label = 1;
-% labels = zeros(x,y);
-% prev_cluster = 0;
-% for i = 1:x
-%     for j = 1:y
-%        if prev_cluster == clusters((i-1)*y+j)
-%             labels(i,j) = clusters((i-1)*y+j); % I shoud append i,j in here
-%        else 
-%            label = label + 1;
-%            labels(i,j) = clusters((i-1)*y+j);
-%        end
-%     previous_cluster = clusters((i-1)*y+j);   
-%     end
-% end
-% figure;imshow(label2rgb(labels));pause;
-% figure;imshow(img);pause;
+label = 1;
+labels = zeros(x,y);
+for i = 1:x
+    for j = 1:y
+            labels(i,j) = clusters((i-1)*y+j); % I shoud append i,j in here
+    end
+end
+labels = labels(labels>0)
+figure;imshow(label2rgb(labels));pause;
+figure;imshow(img);pause;
